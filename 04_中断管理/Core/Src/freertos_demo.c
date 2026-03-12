15546c98286d8375c952b6ddb66803f851710ba8
WF\WF <wf3344520520@126.com>
2026-03-12 11:19:52 +0800

    1.任务挂起和恢复 2.中断管理

---
+++ 04_中断管理/Core/Src/freertos_demo.c
@@ -0,0 +1,80 @@
+#include "freertos_demo.h"
+#include "FreeRTOS.h"
+#include "task.h"
+#include "main.h"
+
+extern uint8_t key_is_pressed; // 声明外部变量key_is_pressed
+
+// 启动任务配置
+#define START_TASK_STACK_DEPTH 128
+#define START_TASK_PRIORITY 1
+TaskHandle_t start_task_handle = NULL;
+void start_task(void *arg);
+
+// 任务1配置
+#define TASK1_STACK_DEPTH 128
+#define TASK1_PRIORITY 2
+TaskHandle_t task1_handle = NULL;
+void task1(void *arg);
+
+/**
+ * @description: 启动FreeRTOS
+ * @return {*}
+ */
+void freertos_start(void)
+{
+  // 1. 创建启动任务
+  xTaskCreate(
+      (TaskFunction_t)start_task,                     // 任务函数
+      (char *)"start_task",                           // 任务名称
+      (configSTACK_DEPTH_TYPE)START_TASK_STACK_DEPTH, // 任务栈大小
+      (void *)NULL,                                   // 任务参数
+      (UBaseType_t)START_TASK_PRIORITY,               // 任务优先级
+      (TaskHandle_t *)&start_task_handle);            // 任务句柄
+  // 2. 启动调度器: 会自动创建空闲任务
+  vTaskStartScheduler();
+}
+
+void start_task(void *arg)
+{
+  /* 进入临界区: 保护临界区代码不会被打断 */
+  taskENTER_CRITICAL();
+
+  // 1. 创建任务1
+  xTaskCreate(
+      (TaskFunction_t)task1,                     // 任务函数
+      (char *)"task1",                           // 任务名称
+      (configSTACK_DEPTH_TYPE)TASK1_STACK_DEPTH, // 任务栈大小
+      (void *)NULL,                              // 任务参数
+      (UBaseType_t)TASK1_PRIORITY,               // 任务优先级
+      (TaskHandle_t *)&task1_handle);            // 任务句柄
+
+  // 删除启动任务，只需要执行一次
+  vTaskDelete(start_task_handle);
+
+  /* 退出临界区 */
+  taskEXIT_CRITICAL();
+}
+
+void task1(void *arg)
+{
+  while (1)
+  {
+    if (key_is_pressed == 1)
+    {
+      // 关中断
+      printf("close interrupts\r\n");
+      portDISABLE_INTERRUPTS();
+      key_is_pressed = 0; // 重置按键状态
+    }
+    else if (key_is_pressed == 2)
+    {
+      // 开中断
+      printf("enable interrupts\r\n");
+      portENABLE_INTERRUPTS();
+      key_is_pressed = 0; // 重置按键状态
+    }
+
+
+  }
+}
---
+++ 04_中断管理/MDK-ARM/06_key_led_flow_register_pro_hal/croutine.d
@@ -0,0 +1,13 @@
+06_key_led_flow_register_pro_hal\croutine.o: ..\FreeRTOS\source\croutine.c
+06_key_led_flow_register_pro_hal\croutine.o: ../FreeRTOS/include/FreeRTOS.h
+06_key_led_flow_register_pro_hal\croutine.o: C:\Program Files\Keil_v5_mdk\ARM\arm5_compiler\Bin\..\include\stddef.h
+06_key_led_flow_register_pro_hal\croutine.o: C:\Program Files\Keil_v5_mdk\ARM\arm5_compiler\Bin\..\include\stdint.h
+06_key_led_flow_register_pro_hal\croutine.o: ../Core/Inc/FreeRTOSConfig.h
+06_key_led_flow_register_pro_hal\croutine.o: ../FreeRTOS/include/projdefs.h
+06_key_led_flow_register_pro_hal\croutine.o: ../FreeRTOS/include/portable.h
+06_key_led_flow_register_pro_hal\croutine.o: ../FreeRTOS/include/deprecated_definitions.h
+06_key_led_flow_register_pro_hal\croutine.o: ../FreeRTOS/portable/RVDS/ARM_CM3/portmacro.h
+06_key_led_flow_register_pro_hal\croutine.o: ../FreeRTOS/include/mpu_wrappers.h
+06_key_led_flow_register_pro_hal\croutine.o: ../FreeRTOS/include/task.h
+06_key_led_flow_register_pro_hal\croutine.o: ../FreeRTOS/include/list.h
+06_key_led_flow_register_pro_hal\croutine.o: ../FreeRTOS/include/croutine.h
